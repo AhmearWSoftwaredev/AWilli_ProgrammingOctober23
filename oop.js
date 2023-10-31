423f327a90c1058de864718a75db74d0cf552297
AhmearWSoftwaredev <Ahmearwilliams22@gmail.com>
2023-10-31 13:42:55 -0400

    More practice w/ promises & async await

--- oop.js
+++ oop.js
@@ -1,72 +1,45 @@
  pokemon = {
-     types : ['Fire','Water','Grass'],
-     names: ['Squirtle', 'Mudkip','Piplup' ]
+    Regions:['Kanto','Hoenn','Sinnoh'],
+    professors: ['Oak', 'Birch','Rowan'],
+    Rivals: ['Gary','May','Barry'],
+    types : ['Fire','Water','Grass'],
+    names: ['Squirtle', 'Mudkip','Piplup','Charmander','Torchic','Chimchar','Bulbasaur','Treecko','Turtwig']
  };
-// const pokemon_name = pokemon.names[2];
-// let chosen_announcement = (time,work) => {
-//     return new Promise(( resolve,reject ) => { 
-//         if (pokemon_name == 'Piplup'){
-//             setTimeout(() => {
-//                 resolve(work())
-//             },time)
-//         }
-//        else{
-//               reject(console.log("fuck off oak isnt in the office"))
-//         }
-//         });
-//     } 
 
-// chosen_announcement(1000, () => console.log(pokemon_name))
-
-// .then(() => {
-//    return chosen_announcement(2000, () => console.log("idek"));
-// })
-
-
-// const nonexistingfunc = function () {
-//     console.log("I exist now bitch ")
-// }
-
-// async function order () {
-//     try{
-//         await nonexistingfunc()
-//     }
-//     catch(error){
-//         console.log("that function doesnt exist bitch ");
-//     }
-//     finally{
-//         console.log("runs anyway")
-//     }
-// }
-
-// order()
-
-let trainer_choice = ()=> {
-    return new Promise((resolve,reject) =>{
-        setTimeout(()=>{
-            resolve(console.log("you chose  " +  `${pokemon.names[1]}`))
-        },2000)
-    }) 
+let pulls_pokemon_type = () => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(console.log("Hello and welcome to the drive thru pokemon center where we expedite your journey."),
+            console.log('You dont get to pick a starter here were in the ' + ` ${pokemon.Regions[1]}`+' region so youll get a'+ ` ${pokemon.types[2]}`+' type today'))
+        },1000)
+    })
 
 }
 
-async function steps (){
-    try{console.log("first")
-    console.log("second")
-    console.log("going to pull from function finish executing then come back")
-    await trainer_choice();
-    console.log("running after pokemon choice is taken in")
-    console.log("await works by taking into accoount a time component which comes from calling a function that" + "uses a promise which inherently introduces a relationship between time and work ")
+let pulls_pokemon_name = () => {
+    return new Promise((resolve,reject) =>{ 
+    setTimeout(() => {
+        resolve(console.log("Since youre getting a" + ` ${pokemon.types[2]}` ,"type today im gonna hand you this" + ` ${pokemon.names[7]}`))
+    },2000)});
+}
+
+
+async function drive_thru_journey(){
+    try{
+        console.log("ugh um...");
+        await pulls_pokemon_type();
+        console.log("do you have a random choice of number between one and three?");
+        console.log("oh so your number is one ?");
+        await pulls_pokemon_name();
     }
     catch(error){
-        console.log("error")
+        console.log("it appears we have no more pokemon")
     }
     finally{
-        setTimeout(() => {console.log("THE END")},2000)
+        console.log("we always want to thank you for coming regardless of outcome! Be well!")
     }
-}
-steps();
+} 
 
-console.log("im running after function call")
-console.log("Im also RUNNING after function call")
+drive_thru_journey()
+
 
